522b0dc6505832d8a2d2f8c0e871fac38c43f00d
Frederick Lahde <fredlahde@gmail.com>
2019-07-09 10:20:38 +0200

    init

---
+++ kops.sh
@@ -0,0 +1,6 @@
+export KOPS_STATE_STORE=do://kubkop
+export DIGITALOCEAN_ACCESS_TOKEN=
+export S3_ENDPOINT=ams3.digitaloceanspaces.com
+export S3_ACCESS_KEY_ID=
+export S3_SECRET_ACCESS_KEY=
+export KOPS_FEATURE_FLAGS="AlphaAllowDO"
